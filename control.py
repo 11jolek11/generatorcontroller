7ec14f06266d5fae0482fc7c8feef93a51c22b34
11jolek11 <dabrowskiandrzej094@gmail.com>
2022-12-01 20:05:56 +0100

    server returns in json format

--- control.py
+++ control.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import subprocess
 import json
 
@@ -9,41 +9,37 @@ class Controller:
         self.app = Flask(__name__)
         self.register = {}
         self._config = ''
-        # TODO: Add UUID instead of id
         @self.app.route('/<id>/start', methods=['post']) 
         def start(id):
             self._config = request.get_json()
             self._config = json.dumps(self._config)
             self.start_generator(id, self._config)
-            # TODO: change to JSON
-            return """Sent"""
+            return jsonify({'req_status': str(id) + ' started'})
 
         @self.app.route('/<id>/stop', methods=['post'])
         def stop(id):
             self.stop_generator(id)
-            # TODO: change to JSON
-            return """Halted"""
+            return jsonify({'req_status': str(id) + ' stopped'})
 
     def start_generator(self, id:str, config:str):
-        # TODO: dodaj funkcje która będzie zmieniać w wartości w czasie działania generatora
-        # TODO: consider adding restriction to one generator per Controler instance
-        # if id in self.register.keys():
-        #     self.register[id].communicate(input=config)
-        #     print('Updating...')
-
-
         if id in self.register.keys():
             self.register[id].kill()
             print('Updating...')
 
         temp = None
         temp = subprocess.Popen(['python', 'generator.py', '--config', config])
-        self.register.update({id: temp})
+        self.register.update({id: temp, 'config': config})
 
-    def stop_generator(self, name:str):
+    def stop_generator(self, id:str):
         print("\033[93m* Generator deactivated\033[0m")
-        self.register[name].kill()
-        del self.register[name]
+        self.register[id].kill()
+        del self.register[id]
+
+    def status_generator(self, id:str):
+        if id in self.register.keys():
+            return jsonify({'id': id, 'status_code': 1, 'status': 'up'})
+        else:
+            return jsonify({'id': id, 'status_code': 0, 'status': 'down'})
 
     def set_up(self, port:int):
         self.app.run(port=port, debug=True)
