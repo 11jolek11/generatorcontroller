dacb6ebb67e8463896f30cc0a9b57b1ad111da3c
11jolek11 <dabrowskiandrzej094@gmail.com>
2022-12-10 07:41:13 +0100

    Agregation works

--- agregator.py
+++ agregator.py
@@ -5,6 +5,8 @@ import requests
 import paho.mqtt.client as mqtt
 import json
 import uuid
+import pandas as pd
+from pandas.io.json import json_normalize
 
 
 class Agregator():
@@ -14,17 +16,55 @@ class Agregator():
         CORS(self.server)
         self.mqtt_client = mqtt.Client(client_id=str(self.uuid), transport='websockets')
 
-        self._conf = {
-            'method': 'HTTP',
-            'target': '',
-            'port': 1883,
+        self.memory_queue = pd.DataFrame()
 
+        self.last_data = [None]
+
+        self.conf = {
+            'method': 'http',
+            'http':{
+                'destiantion': '127.0.0.1',
+                'destiantion_port': 9000
+            },
+            'mqtt': {
+                'broker': 'test.mosquitto.org',
+                'broker_port': 1883,
+                'topic': 'baltazar'
+            }
         }
 
+
+
         # Routing
-        @self.server.route('/')
+        @self.server.route('/', methods=['post'])
         def intercept():
-            pass
+            # TODO: do agragacji danych i ich obróbki użyj dataframe z pandas
+            data = request.form['data']
+            data_json = json.loads(data)
+            # FIXME: find better solution:
+            if len(self.last_data) > 1:
+                if data_json.keys() != self.last_data[-1].keys():
+                    # Clear DataFrame and prepare for next type of data
+                    self.memory_queue = self.memory_queue[0:0]
+            self.last_data.append(data_json)
+            df = json_normalize(data_json)
+            # print('###$')
+            # print(df)
+            if self.memory_queue.empty:
+                self.memory_queue = df
+            else:
+                # print(self.memory_queue)
+                print('#################')
+                print(df)
+                # FIXME: Join on 1st column
+                # self.memory_queue = self.memory_queue.join(df, on=df.columns.values.tolist()[0])
+
+                self.memory_queue= pd.concat([self.memory_queue, df], ignore_index=True)
+                print('#################')
+                print(self.memory_queue)
+            return """HTML"""
+            
+            # print(self.memory_queue)
 
         @self.server.route('/info')
         def info():
@@ -34,15 +74,21 @@ class Agregator():
         def change_config():
             pass
 
+
+        self.server.run(port=9000, debug=True)
+
     # MQTT connection hooks
     @staticmethod
-    def on_connect():
+    def on_connect(client, userdata, flags, rc):
         pass
 
     @staticmethod
-    def on_publish():
+    def on_publish(client, userdata, mid):
         pass
 
     @staticmethod
-    def on_message():
+    def on_message(client, userdata, msg):
         pass
+
+if __name__ == "__main__":
+    Agregator()
